e76c6ae62e47edbfebc87e6f2d14239f43dd3dc2
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-11-12 23:35:44 -0500

    Super rough edits

--- src/containers/Start.js
+++ src/containers/Start.js
@@ -11,8 +11,9 @@ import {
 } from 'rebass'
 import theme, { geo, colors, mx, cx } from '../theme'
 import { Head, Link } from 'react-static'
-import Flag from '../components/Flag'
+import Nav from '../components/Nav'
 import Icon from '../components/Icon'
+import CTA from '../components/CTA'
 import Footer from '../components/Footer'
 
 const Base = Container.extend.attrs({
@@ -23,9 +24,7 @@ const Base = Container.extend.attrs({
 })``
 
 const Header = Box.extend.attrs({
-  pt: [5, 6],
-  pb: [3, 5],
-  px: 3,
+  p: 3,
   color: 'white',
   w: 1
 })`
@@ -33,7 +32,7 @@ const Header = Box.extend.attrs({
   text-align: center;
 `
 
-const Steps = Box.extend`
+const Steps = Box.extend.attrs({ mt: [0, 4] })`
   display: grid;
   grid-template-columns: 1fr;
 
@@ -45,7 +44,7 @@ const StepBox = Flex.extend.attrs({
   direction: 'column',
   justify: 'center',
   my: 0,
-  p: 3
+  px: 3
 })`
   position: relative;
 
@@ -56,8 +55,8 @@ const StepBox = Flex.extend.attrs({
     left: 0;
     z-index: -1;
     width: 100%;
-    height: 50%;
-    background-color: ${props => cx(props.theme)};
+    height: 12rem;
+    background-color: currentcolor;
   }
   &:before { transform: skew(0deg, 10deg) translateX(-49.99%); }
   &:after { transform: skew(0deg,-10deg) translateX(50%); }
@@ -65,30 +64,55 @@ const StepBox = Flex.extend.attrs({
   &:first-child { top: 0; }
 
   ${mx[1]} {
-    &:before, &:after { height: 50%; width: 100%; }
+    &:before, &:after { height: 6rem; }
     &:before { transform: skew(-15deg,0deg) translateY(50%); }
     &:after { transform: skew(15deg,0deg) translateY(-50%); }
   }
 
   h3, p { color: ${colors.white}; }
+  p { line-height: 1.5; }
 `
 const Step = ({ icon, name, desc, ...props }) => (
   <StepBox {...props}>
-    <Icon name={icon} fill="white" size={48} />
-    <Box mt={2}>
+    <Icon name={icon} fill="white" size={36} />
+    <Box mt={1}>
       <Subhead f={4} mb={1} children={name} />
-      <Text f={3} children={desc} />
+      <Text f={2} children={desc} />
     </Box>
   </StepBox>
 )
 
-const Cards = Container.extend.attrs({ px: 3, mt: 3 })`
+const Whys = Container.extend.attrs({ px: 3, py: 4 })`
   display: grid;
   grid-gap: 1rem;
   text-align: left;
 
   ${mx[1]} {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  div {
+    border-radius: .25rem;
+    border: 3px solid ${colors.primary};
+  }
+  p {
+    line-height: 1.75;
+  }
+`
+const Why = ({ name, desc, ...props }) => (
+  <Box p={3} bg="white" {...props}>
+    <Subhead color="black" mt={0} mb={1} f={3} children={name} />
+    <Text color="steel" f={2} m={0} children={desc} />
+  </Box>
+)
+
+const Cards = Container.extend.attrs({ maxWidth: 48 * 16, px: 3, mt: 3 })`
+  display: grid;
+  grid-gap: 1rem;
+  text-align: left;
+
+  ${mx[1]} {
+    grid-template-columns: repeat(2, 1fr);
   }
 
   div {
@@ -109,49 +133,63 @@ const Card = ({ name, desc, ...props }) => (
 export default () => (
   <Provider theme={theme}>
     <Head><title>Start – Hack Club</title></Head>
-    <Flag />
     <Header>
-      <Heading is="h1" f={6}>
-        Let‘s start your club!
+      <Nav />
+      <Heading is="h1" f={6} mt={4}>
+        Start your Hack Club
       </Heading>
-      <Subhead is="h2" f={3} mt={3} caps>
-        What you‘ll get
+      <Subhead is="h2" f={3} mt={3}>
+        Your high school needs a creative space
       </Subhead>
-      <Cards>
-        <Card
-          name="Curriculum"
-          desc="Workshops to get your members started making sites + apps + games"
-        />
-        <Card
-          name="Assistance"
-          desc="Chat with the community on Slack, or talk with a team member for help"
-        />
-        <Card
-          name="Marketing"
-          desc="Strategies, ideas, posters, stickers—whatever you need to get the word out"
-        />
-      </Cards>
+      <CTA is={Link} to="/apply" bg="white" color="primary">Apply »</CTA>
     </Header>
     <Steps>
       <Step
-        theme="orange.5"
+        color="pink.5"
         icon="assignment"
-        name="Apply"
-        desc="Submit your application to get things rolling"
+        name="Submit your application"
+        desc="Answer a few questions and get the ball rolling"
       />
       <Step
-        theme="pink.5"
+        color="green.5"
         icon="ring_volume"
         name="Train with us"
-        desc="Talk and get tips from our team"
+        desc="On a call we‘ll get to know you and get you going"
       />
       <Step
-        theme="blue.5"
+        color="blue.5"
         icon="local_activity"
-        name="Start your club"
-        desc="This is the best part!"
+        name="Lead your club"
+        desc="This is the fun part! You‘ll begin planning, marketing, etc"
       />
     </Steps>
+    <Heading f={4} color="primary" mt={4} center>
+      Why start a coding club?
+    </Heading>
+    <Whys>
+      <Why
+        name="Coding is uniquely empowering."
+        desc="When you’re coding, it doesn’t matter who you are—you can be a high schooler with a laptop making an app millions of people use."
+      />
+      <Why
+        name="Hack Club isn’t just ”programming.”"
+        desc="Computer science classes focus on programming principles, not making projects. Hack Club does things differently."
+      />
+    </Whys>
+    <Cards>
+      <Card
+        name="Curriculum"
+        desc="Workshops to get your members started making sites + apps + games"
+      />
+      <Card
+        name="Assistance"
+        desc="Chat with the community on Slack, or talk with a team member for help"
+      />
+      <Card
+        name="Marketing"
+        desc="Strategies, ideas, posters, stickers—whatever you need to get the word out"
+      />
+    </Cards>
     <Footer />
   </Provider>
 )
